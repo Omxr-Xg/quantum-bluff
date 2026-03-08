4b7934c46e2d8e2be5ca4952a6b59eb37752fdd5
Elhadj BAH <elhadj-abdoulaye.bah@etu.unistra.fr>
2026-03-08 15:12:40 +0100

    I didn't tested the repositories so I take the chance to do it. And with some changes, the repositories work well

--- server/src/repositories/gameHistory.repository.ts
+++ server/src/repositories/gameHistory.repository.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../services/db.service";
 
 export interface HandSummary {
   tableId: string;
