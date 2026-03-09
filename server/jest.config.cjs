09afb2d1732ded33baa12b57f016b20fb6c53cd9
Omar AGUECH <omxxr@Omars-MacBook-Air.local>
2026-03-09 01:52:44 +0100

    test: ignore old test folders in jest config

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -1,13 +1,19 @@
-// server/jest.config.cjs
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/tests.old/',      // ← Ignorer les anciens tests
+    '/services.old/'     // ← Ignorer les anciens services
+  ],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
     '!src/**/__tests__/**',
+    '!src/tests.old/**',    // ← Ignorer aussi de la couverture
+    '!src/services.old/**'
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'cobertura'],
