3275d2f3e0594c2cb8808f86b5cac7b581e225f1
BAOUAB LINDA <linda.baouab@etu.unistra.fr>
2026-03-11 21:49:40 +0000

    Update 3 files
    - /.gitlab-ci.yml
    - /server/jest.config.cjs
    - /server/tsconfig.json

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -1,27 +1,53 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],  // ← Utiliser src, pas dist
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],  // ← Fichiers .ts
+
+  roots: ['<rootDir>/src'],
+
+  testMatch: ['**/__tests__/**/*.test.ts'],
+
   testPathIgnorePatterns: [
     '/node_modules/',
     '/tests.old/',
-    '/services.old/'
+    '/services.old/',
+    '/dist/',
+    '/src/generated/'
   ],
+
+  extensionsToTreatAsEsm: ['.ts'],
+
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: '<rootDir>/tsconfig.json'
+      }
+    ]
+  },
+
+  moduleFileExtensions: ['ts', 'js', 'json'],
+
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
     '!src/**/__tests__/**',
     '!src/tests.old/**',
-    '!src/services.old/**'
+    '!src/services.old/**',
+    '!src/generated/**',
+    '!src/run-tests.manual.ts',
+    '!src/test-utils.ts'
   ],
+
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'cobertura'],
+
   reporters: [
     'default',
     ['jest-junit', { outputDirectory: './', outputName: 'junit.xml' }]
-  ],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',  // ← Supprime l'extension .js dans les imports
-  },
+  ]
 };
