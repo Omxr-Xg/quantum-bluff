eade3f9be0ba6d48a9277d655fee2eccae3b98c2
Omar AGUECH <omxxr@Omars-MacBook-Air.local>
2026-03-12 15:36:22 +0100

    feat: wrap app with SocketProvider for global socket access

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { SocketProvider } from './contexts/SocketContext'  // ← AJOUTER CETTE LIGNE
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <SocketProvider>  {/* ← AJOUTER CETTE LIGNE */}
+        <App />
+      </SocketProvider>  {/* ← AJOUTER CETTE LIGNE */}
     </Provider>
   </React.StrictMode>,
 )
