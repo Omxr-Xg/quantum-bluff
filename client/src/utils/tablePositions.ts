0305d72085c11a1ac81042539c30897aebdb1ffa
abayrak <azra.bayrak@etu.unistra.fr>
2026-04-19 21:53:19 +0200

    augementer l emplacement de la table

--- client/src/utils/tablePositions.ts
+++ client/src/utils/tablePositions.ts
@@ -16,9 +16,9 @@ export function calculatePlayerPositions(
   _isTablet = false
 ) {
   const radiusX = isMobile ? 180 : 451;
-  const radiusY = isMobile ? 320 : 220;
+  const radiusY = isMobile ? 280 : 220;
 
-  const startAngle = Math.PI / 2;
+  const startAngle = isMobile ? Math.PI / 2 + 0.15 : Math.PI / 2;
   const angleStep  = (2 * Math.PI) / count;
 
   return Array.from({ length: count }, (_, position) => {
