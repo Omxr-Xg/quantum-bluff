58e3e9a4a22f2494b84b25e510bb44fcd98dcfe1
Linda Baouab <linda.baouab@etu.unistra.fr>
2026-04-27 18:26:01 +0200

    remonter la table

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -3300,7 +3300,7 @@ export function Game() {
          {/* TABLE */}
         <div
         ref={tourRefTable}
-        className={`flex-1 flex items-center justify-center relative ${isMobile ? 'px-4 pt-0 w-full -mt-8' : 'px-6 pt-32 min-h-[600px]'}`}
+        className={`flex-1 flex items-center justify-center relative ${isMobile ? 'px-4 pt-0 w-full -mt-8' : 'px-6 pt-0 min-h-[600px] -translate-y-8'}`}
         >
         <PokerTable
         players={tablePlayers}
